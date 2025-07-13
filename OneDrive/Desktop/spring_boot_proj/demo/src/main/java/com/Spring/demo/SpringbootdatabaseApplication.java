82ceb8a5dd058baa37e295a710020f95fd9e38ba
𝙰𝚈𝙼𝙰𝙽 𝙰𝙻𝙹𝙰𝙼𝙰𝙻 <ayman.aljamal2017@gmail.com>
2025-07-13 15:10:27 +0300

    First SpringBoot program with Database

---
+++ OneDrive/Desktop/spring_boot_proj/demo/src/main/java/com/Spring/demo/SpringbootdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdatabaseApplication.class, args);
+	}
+
+}
